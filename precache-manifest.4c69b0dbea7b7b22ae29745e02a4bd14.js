4d495974809536941c932ea5fb3d535c7e053c52
anshul <anshul.nautiyal1@wipro.com>
2019-01-26 21:04:52 +0530

    Updates

---
+++ precache-manifest.4c69b0dbea7b7b22ae29745e02a4bd14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d9f5e8903a243d0325",
+    "url": "/to-do-list/static/css/main.e49def61.chunk.css"
+  },
+  {
+    "revision": "23d9f5e8903a243d0325",
+    "url": "/to-do-list/static/js/main.23d9f5e8.chunk.js"
+  },
+  {
+    "revision": "723041c1909323bbf567",
+    "url": "/to-do-list/static/css/1.e415f9dd.chunk.css"
+  },
+  {
+    "revision": "723041c1909323bbf567",
+    "url": "/to-do-list/static/js/1.723041c1.chunk.js"
+  },
+  {
+    "revision": "af0bf1497ad58103b57b",
+    "url": "/to-do-list/static/js/runtime~main.af0bf149.js"
+  },
+  {
+    "revision": "16649b71fbd7521277f6b00162c281ef",
+    "url": "/to-do-list/index.html"
+  }
+];
